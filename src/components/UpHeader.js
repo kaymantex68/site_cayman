1c901110716c0e24fbdf41279e3eb8e6b005e296
Home <kaymantex68@yandex.ru>
2020-11-19 23:35:39 +0300

    home доработки по слайдеру

--- src/components/UpHeader.js
+++ src/components/UpHeader.js
@@ -6,7 +6,7 @@ function UpHeader() {
         <div className={classes.Main}>
             <div className={classes.Mail}><span className={classes.span_info}>mail@kaymantex.ru</span></div>
             <div className={classes.Phone}><span className={classes.span_info}>Тел: +7 (920) 489-74-37</span></div>
-            <div className={classes.Info}></div>
+            <div className={classes.Info}><span className={classes.span_info}>Тамбов, ул.Агапкина 25А</span></div>
         </div>
     )
 };
