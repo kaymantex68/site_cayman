61899c72270cb0d2f0fa5ad5e0d3145d2c7527de
Andrey Sergeevich <kaymantex68@yandex.ru>
2020-12-11 14:38:56 +0300

    office

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import classes from './Catalog.module.css'
 import CatalogContainer from './CatalogContainer'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 
 function Catalog({ match }) {
+
     return (
         <>
             <div className={classes.Main}>
                 <div className={classes.Manual}>
                     <div className={classes.Direct}>
                         <span className={classes.FastDirect}>каталог</span>
-
+                        {console.log(match)}
                         {Object.keys(match.params).map((item, index) => {
                             return (
                                 <>
                                     <span className={classes.FastDirectDot}> ᐅ </span>
-                                    <NavLink key={`cat-nav-${index}`} to="#"><span key={`cat-sp-${index}`} className={classes.FastDirect}>{match.params[item]}</span></NavLink>
+                                    <NavLink to={`/catalog/${Object.keys(match.params).map((item, index) => {
+                                      
+                                        return (`${match.params[item]}/`);
+                                    })
+                                        }`} key={`cat-nav-${index}`}><span key={`cat-sp-${index}`} className={classes.FastDirect}>{match.params[item]}</span></NavLink>
 
                                 </>
                             )
