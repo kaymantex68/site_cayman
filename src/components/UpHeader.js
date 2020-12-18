80aa74397e1935a29050a27721aa132b5480fe27
Andrey Sergeevich <kaymantex68@yandex.ru>
2020-12-18 15:50:15 +0300

    office

--- src/components/UpHeader.js
+++ src/components/UpHeader.js
@@ -5,8 +5,9 @@ function UpHeader() {
     return (
         <div className={classes.Main}>
             <div className={classes.Container}>
-                <div className={classes.Mail}><span className={classes.span_info}>mail@kaymantex.ru</span></div>
-                <div className={classes.Phone}><span className={classes.span_info}>Тел: +7 (920) 489-74-37</span></div>
+                <div className={classes.Mail}><span className={classes.span_info}>info@kaymantex.ru</span></div>
+                <div className={classes.Phone}><span className={classes.span_info}>Тел: 8 (4752) 42-47-27</span> <span className={classes.span_info}>Тел: +7 920 233-34-34</span> </div>
+                
                 <div className={classes.Info}><span className={classes.span_info}>Тамбов, ул.Агапкина 25А</span></div>
             </div>
         </div>
