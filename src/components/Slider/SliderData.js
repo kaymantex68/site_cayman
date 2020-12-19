4686a8024243ac01da225f9d218ac9c66c61cee4
Home <kaymantex68@yandex.ru>
2020-12-19 22:54:43 +0300

    home slider

--- src/components/Slider/SliderData.js
+++ src/components/Slider/SliderData.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import i1 from './PicSlider/slide1.png';
 import slide1_fon from './PicSlider/slide1_fon.png';
-import i2 from './PicSlider/2.png';
+import i2 from './PicSlider/1.png';
 import slide2_fon from './PicSlider/slide5_fon.jpg';
 import i3 from './PicSlider/3.png';
+import slide3_fon from './PicSlider/slide6_fon.jpg';
 
 export const SidebarData=[
     {
@@ -26,19 +27,19 @@ export const SidebarData=[
         
     },
     {
-        title: 'IP купольная',
+        title: 'Ranger 2',
         backColor: '#78C47A',
-        backPicture: slide2_fon,
+        backPicture: slide3_fon,
         colorTitle: 'white',
         colorText: 'white',
         path: '/products',
         pic: i2,
         info: {
-            text: 'Разрешение: 5mpx',
-            text1: 'Вариофокальный объектив: 3,6mm',
+            text: 'Разрешение: 2mpx',
+            text1: 'PTZ: 360',
             text2: 'Матрица: Sony IMX 572',
             text3: 'Процессор: Ambarella',
-            text4: 'Водонепроницаемость: IP66',
+            text4: 'SD card: 256Gb',
             text5: 'Подсветка: 15м'
         },
         coast: '5590₽',
@@ -68,7 +69,7 @@ export const SidebarData=[
     {
         title: 'Какая то еще камера',
         backColor: '#78C47A',
-        backPicture: slide2_fon,
+        backPicture: slide3_fon,
         colorTitle: 'white',
         colorText: 'white',
         path: '/products',
