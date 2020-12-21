a70d7ebd692a2b260a12a0eeb771a03c46578229
Andrey Sergeevich <kaymantex68@yandex.ru>
2020-12-22 00:17:37 +0300

    uvarovo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
 
+
+  ReactGA.initialize('G-QYDKQ70GM0');
+ 
+    ReactGA.pageview('/');
+ 
 
 ReactDOM.render(<App />,document.getElementById('root'));
