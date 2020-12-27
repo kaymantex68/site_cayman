2d6c767a73aaaac1f3b117a48a7c3c8c3610d087
Home <kaymantex68@yandex.ru>
2020-12-27 11:43:46 +0300

    home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { BrowserRouter, Switch, Route, Link, useLocation } from 'react-router-dom'
 import classes from './App.module.css';
 import UpHeader from './components/UpHeader';
@@ -12,6 +12,7 @@ import DetailProduct from './components/DetailProduct/DetailProduct';
 import Contacts from './components/Contacts/Contacts';
 import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
+import { Helmet } from 'react-helmet'
 
 
 
@@ -19,10 +20,20 @@ import { createBrowserHistory } from 'history';
 
 function App() {
 
+  const { Data } = useContext(Context);
+  let DataCatalog = [];
+
+  Data.map((item) => {
+    return (
+      DataCatalog.push(item)
+    )
+  })
+
+
   var location = useLocation();
-    // console.log('pathname---------------APP');
-    // console.log(location.pathname);
- 
+  // console.log('pathname---------------APP');
+  // console.log(location.pathname);
+
   ReactGA.initialize('UA-185966908-1');
   useEffect(() => {
     // ReactGA.ga('send', 'pageview', '/');
@@ -33,45 +44,70 @@ function App() {
     // ReactGA.pageview('/:model');
     ReactGA.pageview(location.pathname);
   }, [location]);
-  
-  
+
+
 
   return (
 
 
     <>
-      
+      <Helmet>
+        <meta charset="utf-8" />
 
-        <div className={classes.Main}>
-          <div className={classes.Header}>
-            <UpHeader />
-          </div>
-          <div className={classes.Navbar}>
-            <Navbar />
-          </div>
-          <div className={classes.Content_Rout}>
-            <Switch>
-              <Route exact path="/" component={Main} />
-              <Route exact path="/contacts/adress" component={Contacts} />
-              <Route exact path="/:model" component={DetailProduct} />
-              <Route exact path="/catalog/all" component={Catalog} />
-              <Route exact path="/catalog/all/:model" component={DetailProduct} />
-              <Route exact path="/catalog/:cat" component={Catalog} />
-              <Route exact path="/catalog/:cat/:type" component={Catalog} />
-              <Route exact path="/catalog/:cat/:type/:brand" component={Catalog} />
-              <Route exact path="/catalog/:cat/:type/:brand/:model" component={DetailProduct} />
-              {/* <Route path="*" component={Main} /> */}
-            </Switch>
-          </div>
-          <Footer />
+        <meta name="yandex-verification" content="af6bf6599fd7f67e" />
+        <meta name="google-site-verification" content="ZREAVCIqBSZg9W4q6hxEtVqggkIYbDP1o5fAWHsNmN0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="keywords" content="Продажа и монтаж систем видеонаблюдения. Пожарная и охранная сигнализация. ОПС. СКУД. Монтаж домофонов. Монтаж контроля доступа. Тамбов, тамбовская область.Моршанск, Кирсанов, Жердевка, Рассказово, Уварово, Умет, Мордово. В Тамбове."/>
+        <meta name="rights" content="Кайман 2020"/>
+        <meta name="description" content="Продажа и монтаж систем видеонаблюдения. Пожарная и охранная сигнализация. ОПС. СКУД. Монтаж домофонов. Монтаж контроля доступа. Тамбов, тамбовская область.Моршанск, Кирсанов, Жердевка, Рассказово, Уварово, Умет, Мордово. В Тамбове."/>
+        {DataCatalog.map((item, index) => {
+          return(
+          <meta
+            name="description"
+            content={`${item.brand} ${item.model}`}
+          />)
+        })}
+        
+        <script src="https://kit.fontawesome.com/d8de2d9986.js"></script>
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+
+
+        <title>Кайман. Системы безопасности.</title>
+      </Helmet>
+
+      <div className={classes.Main}>
+        <div className={classes.Header}>
+          <UpHeader />
         </div>
-        {/* <div className={classes.FooterContainer}> */}
+        <div className={classes.Navbar}>
+          <Navbar />
+        </div>
+        <div className={classes.Content_Rout}>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/contacts/adress" component={Contacts} />
+            <Route exact path="/:model" component={DetailProduct} />
+            <Route exact path="/catalog/all" component={Catalog} />
+            <Route exact path="/catalog/all/:model" component={DetailProduct} />
+            <Route exact path="/catalog/:cat" component={Catalog} />
+            <Route exact path="/catalog/:cat/:type" component={Catalog} />
+            <Route exact path="/catalog/:cat/:type/:brand" component={Catalog} />
+            <Route exact path="/catalog/:cat/:type/:brand/:model" component={DetailProduct} />
+            {/* <Route path="*" component={Main} /> */}
+          </Switch>
+        </div>
+        <Footer />
+      </div>
+      {/* <div className={classes.FooterContainer}> */}
 
-        {/* </div> */}
+      {/* </div> */}
 
-     
-    
-</>
+
+
+    </>
 
   );
 };
