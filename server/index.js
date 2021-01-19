e5ca3d42ae83a4d390effdd2003f71f69d100930
Andrey Sergeevich <kaymantex68@yandex.ru>
2021-01-19 17:06:47 +0300

    office

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 
 async function getDATA(message){
@@ -26,7 +26,7 @@ async function getDATA(message){
     })
 }
 
-app.post('/SendOrder', cors(), (req, res) => {
+app.post('/SendOrder', (req, res) => {
    
     const information = req.body;
     console.info('INFORMATION: ', information.data)
