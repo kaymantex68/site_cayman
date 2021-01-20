43e4eded111af6a4bda44ca36a0a2f930e0ae559
Home <kaymantex68@yandex.ru>
2021-01-20 08:07:02 +0300

    home proxy

--- server/index.js
+++ server/index.js
@@ -21,7 +21,13 @@ app.use(function (req, res, next) {
     }
     next();
 });
-
+app.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:3001"],
+        optionsSuccessStatus: 200
+    })
+);
 
 
 
